8f69cc8aa84b01534f4dbf35160034c561d367f9
tccyp <tccyp86@gmail.com>
2023-10-08 01:56:48 -0700

    fix port

--- frontend/src/common.tsx
+++ frontend/src/common.tsx
@@ -1,5 +1,5 @@
-//export const baseUrl = 'http://localhost:8080/';
+// export const baseUrl = 'http://localhost:8000/';
 //export const baseUrl = window.location.href.slice(-1) === '/'? window.location.href + '' : window.location.href + '/'; // http://3.16.155.160:8000/
 
-export const baseUrl = "http://3.16.155.160:8000/";
+export const baseUrl = "http://3.16.155.160/";
 
